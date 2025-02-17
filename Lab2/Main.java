55afb40ade49ec2196eb9c88896ccd85f69e77d7
AdonaiHP <s01309074@acad.tri-c.edu>
2025-02-17 11:42:41 -0500

    Completed CodMain.java
    Added actual code changes to this one

--- Lab2/Main.java
+++ Lab2/Main.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 import java.util.Iterator;
 
 // Lab 2 - Lists, Stacks, and Queues
-// Your Name: 
+// Your Name: Adonai Hawkins-Prather 
 // Notes: The instructions are found in comments through the code starting with "Step n"
 //        Please leave the comments and add your code just after the comment.
 //        Use the examples in the book to complete the lab.
@@ -22,56 +22,95 @@ class Main {
 
 class Lab2 {
   public void linkedList() {
-    // 1. Create a LinkedList<String> object called progLanguages
+        System.out.println("    PART ONE: Iterator Changes ");
+        System.out.println("-----------------------------------");
+  // 1. Create a LinkedList<String> object called progLanguages
+    LinkedList<String> progLanguages = new LinkedList<String>();
 
-    // 2. Add elements to progLanguages: "Java", "Python", "JavaScript", "C++"
+  // 2. Add elements to progLanguages: "Java", "Python", "JavaScript", "C++"
+    progLanguages.add("Java");
+    progLanguages.add("Python");
+    progLanguages.add("JavaScript");
+    progLanguages.add("C++");
 
-    // 3. Remove the element "C++" from the list using .remove()
+  // 3. Remove the element "C++" from the list using .remove()
+    progLanguages.remove("C++");
+        progLanguages.forEach(e -> System.out.print(e.toUpperCase() + " "));
+      System.out.println();
 
-    // 4. Add an element "HTML" at index 2.
+  // 4. Add an element "HTML" at index 2.
+    progLanguages.add(2, "HTML");
 
-    // 5. Iterate over progLanguages and use println() to output each element. You must create an Iterator<string> and use hasNext(), and next() similar to the example in our book.
-    
+  // 5. Iterate over progLanguages and use println() to output each element. You must create an Iterator<string> and use hasNext(), and next() similar to the example in our book.
+  Iterator<String> iterator = progLanguages.iterator();
+    while (iterator.hasNext()) {
+      System.out.print(iterator.next().toUpperCase() + " ");
+    }
+      System.out.println(); 
   }
-  
+    
   public void queue() {
-    // 6. Create a Queue<String> called q
+      System.out.println(" ");
+      System.out.println("    PART TWO: Queue Changes ");
+      System.out.println("-----------------------------------");
+  // 6. Create a Queue<String> called q
+    Queue<String> q = new LinkedList<String>();
+  // 7. Add 5 first names to q.
+    q.add("Adonai");
+    q.add("Amus");
+    q.add("Abigale");
+    q.add("Adrian");
+    q.add("Arnold");
+  // 8. Uncomment the following line
+    System.out.println("Elements of queue: " + q);
+  // 9. Remove the head of the queue and assign it to a String variable removedElement.
+  //    Display the value of "Removed element: " + removedElement
+    String removedElement = q.remove();
+    System.out.println("Removed element: " + removedElement);
 
-    // 7. Add 5 first names to q.
+  // 10. View the head of the queue using peek(). Output it's value.
+    System.out.println("Head of queue: " + q.peek());
 
-    // 8. Uncomment the following line
-    //System.out.println("Elements of queue: " + q);
+  // 11. Using for(String element : q), output all of the values in the queue
+    for(String element : q) {
+        System.out.print(element + " ");
+    }
+    System.out.println();
 
-    // 9. Remove the head of the queue and assign it to a String variable removedElement.
-    //    Display the value of "Removed element: " + removedElement
-
-    // 10. View the head of the queue using peek(). Output it's value.
-
-    // 11. Using for(String element : q), output all of the values in the queue
-
-    
+  System.out.println(" ");
   }
 
-  public void stack() {
-    // 12. Create a Stack<String> called bookStack
+public void stack() {
+      System.out.println("    PART THREE: bookStack Changes ");
+      System.out.println("-----------------------------------");
+  // 12. Create a Stack<String> called bookStack
+    Stack<String> bookStack = new Stack<String>();
+  
+  // 13. Push the following book titles onto bookStack: "Clean Code", "Design Patterns", "Pragmatic Programmer"
+    bookStack.push("Clean Code");
+    bookStack.push("Design Patterns");
+    bookStack.push("Pragmatic Programmer");
 
-    // 13. Push the following book titles onto bookStack: "Clean Code", "Design Patterns", "Pragmatic Programmer"
+  // 14. pop() 1 book off the stack. Display it's value
+    String removedBook = bookStack.pop();
+    System.out.println("Removed book: " + removedBook);
+  
+  // 15. Use the peek() method to view the top book on the stack
+    System.out.println("Top book after pop: " + bookStack.peek());
+  
+  // 16. push() "Web DB Technologies" onto the stack
+    bookStack.push("Web DB Technologies");
 
-    // 14. pop() 1 book off the stack. Display it's value
-
-    // 15. Use the peek() method to view the top book on the stack
-
-    // 16. push() "Web DB Technologies" onto the stack
-
-    // 17. Use the peek() method to view the top book on the stack
+  // 17. Use the peek() method to view the top book on the stack
+    System.out.println("Top book after pop: " + bookStack.peek());
 
     // 18. Search for "Design Patterns" in the stack. Display the results of the search.
+    System.out.println("Does stack contain Design Patterns? " + bookStack.search("Design Patterns"));
+  // 19. Call empty(). Output the results
+    System.out.println("Is stack empty? " + bookStack.empty());
 
-    // 19. Call empty(). Output the results
-
-    // 20. Print the titles of all of the books on the stack
+  // 20. Print the titles of all of the books on the stack
+    System.out.println("All books on stack: " + bookStack);
     
   }
-
-  
 }
