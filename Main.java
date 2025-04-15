c42d1350075e13d1f2a6626c774f750dfd833f11
AdonaiHP <s01309074@acad.tri-c.edu>
2025-04-15 12:45:15 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,30 @@
-//import java.util.*;
-/*
- * IT-2660 - Lab 1
- * Student Name: 
- */
-
-public class Main {
+class Main {
   public static void main(String[] args) {
-    System.out.println("hello, world!");
 
-    Lab1 lab = new Lab1();
-    System.out.println(lab.increment(1));
-  }
-}     
+    MyHashMap<String, Integer> creditHours = new MyHashMap<String, Integer>();
+    // Adding course credit hours
+    creditHours.put("IT-1025", 3);
+    creditHours.put("IT-1050", 3);
+    creditHours.put("IT-1150", 3);
+    creditHours.put("IT-2310", 3);
+    creditHours.put("IT-2320", 4);
+    creditHours.put("IT-2351", 4);
+    creditHours.put("IT-2650", 4);
+    creditHours.put("IT-2660", 4);
+    creditHours.put("IT-2030", 4);
+    // Check for specific values
+    System.out.println("Contains IT-1025: " + creditHours.containsKey("IT-1025"));
+    System.out.println("Contains IT-2110: " + creditHours.containsKey("IT-2110"));
+    // Print all values
+    System.out.println("\nAll values in the map:");
+    System.out.println(creditHours.toString());
+    // Remove specified entries
+    creditHours.remove("IT-2030");
+    creditHours.remove("IT-1150");
+    // Print all values after removal
+    System.out.println("\nValues after removal:");
+    System.out.println(creditHours.toString());
+    }
+    }
 
-// Add all of the methods here
-class Lab1 {
-  public int increment(int num) {
-    return ++num;
-  }
-}
+
